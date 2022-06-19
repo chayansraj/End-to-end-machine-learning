5f39b1eec937c37b21b0f2ab67441a60cf632d7c
CHAYAN SHRANG RAJ <chayansraj95@gmail.com>
2022-06-19 12:08:30 +0200

    nn implement

--- utils.py
+++ utils.py
@@ -24,6 +24,33 @@ def change_path(path):
 
 
 
+### Function to print training and validation curves for neural networks
+import matplotlib.pyplot as plt
+def plot_results(history):
+
+    val_loss = history.history['val_loss']
+    acc = history.history['accuracy']
+    loss = history.history['loss']
+    val_acc = history.history['val_accuracy']
+
+    plt.figure(figsize=(10,4))
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.plot(loss)
+    plt.plot(val_loss)
+    plt.legend(['Training','Validation'])
+
+    plt.figure(figsize=(10,4))
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.plot(acc)
+    plt.plot(val_acc)
+    plt.legend(['Training','Validation'])
+
+    plt.show()
+
+
+
 ### Function for data visualization
 
 
